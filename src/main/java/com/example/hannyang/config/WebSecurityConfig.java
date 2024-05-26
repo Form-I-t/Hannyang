ceb5937a86876afc76a2ccb5cbb724365437bc89
minguk0825 <mapletrue01@naver.com>
2024-05-26 20:04:54 +0900

    cors 설정 추가_4

--- src/main/java/com/example/hannyang/config/WebSecurityConfig.java
+++ src/main/java/com/example/hannyang/config/WebSecurityConfig.java
@@ -27,19 +27,17 @@ public class WebSecurityConfig {
                 .cors().and() // CORS 설정 추가
                 .csrf().disable() // CSRF 토큰 비활성화
                 .authorizeHttpRequests(authz -> authz
-                        // 해당 요청에 대해 아무나 승인
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/api-docs/**", "/product/**", "/api/v1/member/email/**", "/api/v1/member/nickname/**",
                                 "/product/all-products", "/product/products-by-category", "/survey-history/by-survey-number/", "/survey/value-for-money", "/survey/popular",
                                 "/survey/paged", "/survey/latest", "/survey/by-points", "/survey/by-deadline", "/api/v1/auth/signup", "/api/v1/auth/login").permitAll()
-                        // 관리자만 접근할 수 있는 경로 설정
                         .requestMatchers("/product/create-product", "/survey-history/admin/**", "/product-history/change-status", "/product-history/all").hasRole("ADMIN")
-                        // 기타 모든 요청에 대해 승인된 사용자만 허용
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용하지 않음
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(this.jwtTokenFilter, UsernamePasswordAuthenticationFilter.class) // JWT 필터 추가
+                .addFilterBefore(this.jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class) // CORS 필터 추가
                 .build();
     }
 
